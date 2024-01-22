69229842309fb4fa40a36438d5aeb92bba94150f
Jabess7 <jabess7@gmail.com>
2024-01-22 01:29:21 -0300

    3_js

--- JavaScript/2_operadores/script.js
+++ JavaScript/2_operadores/script.js
@@ -25,7 +25,7 @@ console.log(typeof'4555')
 // 5 - simbolos especias em string
 
 console.log("Testando a \n quebrada de linha")
-console.log("espaçamento \t \b de tab")
+console.log("espaçamento \t  de tab")
 
 //6 - concatenação
 
@@ -34,3 +34,64 @@ console.log('oi, ' + "tudo bem" + " ?")
 //7 - templates Strings
 
 console.log(`A soma de 2 + 2 é : ${2+2}`)
+
+// 8 - Boolean
+
+console.log(typeof true)
+
+console.log(5 > 2)
+
+console.log(10 < 5)
+
+// 9 - comparaçoes
+
+console.log(5 <= 5)
+
+console.log(5>5)
+
+console.log(10 == 10)
+ console.log(10 == 9)
+ console.log(10 != 9)
+
+ // 10 - idêntico
+
+ console.log(9 == "9")
+
+ console.log(9 === "9")
+
+ console.log(9 !== "9")
+
+ // 11 - Operadores logicos
+
+ console.log(10 == 10 && 9 == 7)
+
+ console.log(true && true)
+ console.log(5 > 2 && 2 < 10)
+
+ console.log(10 == 10 || 9 == 7)
+
+ //12 - EMPTY VALUES
+
+ console.log(typeof null, typeof undefined)
+
+ console.log(null === undefined)
+ console.log(null == undefined)
+ console.log(null == true, "1")
+ console.log(undefined == false)
+
+
+ // 13 - Conversão de tipo automática
+
+ console.log(5 * null)
+
+ console.log("5" - 3)
+
+ console.log("teste" * "opa")
+
+ console.log("10" + 1)
+
+ console.log(typeof("10" - 1))
+
+  
+
+
---
+++ JavaScript/3_estruturas/script.js
@@ -0,0 +1,11 @@
+// 1 - variáveis
+let nome = "Matheus";
+console.log(nome)
+nome = "Matheus Bastisti"
+console.log(nome)
+
+const idade = 31
+console.log(idade)
+
+// 2 - Mais sobre variáveis
+
