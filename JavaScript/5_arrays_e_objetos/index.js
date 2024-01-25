fec8126a6ba76c86fda03e41344651d5242febe5
Jabess7 <jabess7@gmail.com>
2024-01-25 10:08:29 -0300

    array_objetos

---
+++ JavaScript/5_arrays_e_objetos/index.js
@@ -0,0 +1,19 @@
+// 1 arrays
+
+const lista = [1,1,2,3,4,5,6]
+
+console.log(lista)
+
+console.log(typeof lista)
+const itens = ['matheus',true,2,4.12, []]
+
+console.log(itens)
+
+
+// 2 -
+
+// 3 - propriedades
+
+const numbers = [5,3,4];
+console.log(numbers.length)
+console.log(numbers)
