ec4c9c8fc5ab4f8752904eb3db5bf11baa4ce7e3
Jabess7 <jabess7@gmail.com>
2024-03-09 12:59:56 -0300

    atualizção do curso

--- React/10_Quiz/quiz/src/components/Welcome.jsx
+++ React/10_Quiz/quiz/src/components/Welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = () => {
     <div id="welcome">
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar</p>
-        <button onClick={() => dispatch({type:"CHANGE_STATE"})}>iniciar</button>
+        <button>iniciar</button>
         <img src={Quiz} alt="Inicio do quiz" />
     </div>
   )
